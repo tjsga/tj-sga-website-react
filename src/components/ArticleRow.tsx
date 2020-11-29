259e62e96da345050e7030d1c83a1d1186af9cdb
Michael Fatemi <myfatemi04@gmail.com>
2020-11-29 01:37:09 -0500

    Added relevant CSS

--- src/components/ArticleRow.tsx
+++ src/components/ArticleRow.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import imageUrl from '../imageUrl';
 import BlockContent from '@sanity/block-content-to-react';
 import { Link } from 'react-router-dom';
+import "../css/article.css";
 
 export default function ArticleRow({
 	article,
