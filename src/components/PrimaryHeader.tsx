4a0d32a42ed509b331cc93d3a70a4626ac9608eb
Michael Fatemi <myfatemi04@gmail.com>
2021-01-11 19:25:45 -0500

    Add h1 props to PrimaryHeader

--- src/components/PrimaryHeader.tsx
+++ src/components/PrimaryHeader.tsx
@@ -1,7 +1,18 @@
+// from node_modules/react/index.d.ts: global -> JSX -> IntrinsicElements
+type h1Props = React.DetailedHTMLProps<
+	React.HTMLAttributes<HTMLHeadingElement>,
+	HTMLHeadingElement
+>;
+
 export default function PrimaryHeader({
 	children,
+	...props
 }: {
 	children: React.ReactNode;
-}) {
-	return <h1 className='my-4'>{children}</h1>;
+} & h1Props) {
+	return (
+		<h1 className='my-4' {...props}>
+			{children}
+		</h1>
+	);
 }
