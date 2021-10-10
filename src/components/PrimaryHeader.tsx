2dda93e0b96881c4b3068ffef34b9f9c504b71cb
Michael Fatemi <myfatemi04@gmail.com>
2021-10-09 23:20:26 -0400

    remove unnecessary code

--- src/components/PrimaryHeader.tsx
+++ src/components/PrimaryHeader.tsx
@@ -1,15 +1,9 @@
-// from node_modules/react/index.d.ts: global -> JSX -> IntrinsicElements
-type h1Props = React.DetailedHTMLProps<
-	React.HTMLAttributes<HTMLHeadingElement>,
-	HTMLHeadingElement
->;
-
 export default function PrimaryHeader({
 	children,
 	...props
 }: {
 	children: React.ReactNode;
-} & h1Props) {
+} & JSX.IntrinsicElements['h1']) {
 	return (
 		<h1 className='my-4' {...props}>
 			{children}
