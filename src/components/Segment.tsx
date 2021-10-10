1099f4ad75f9ed8a1b0eaa92401d2250d0a68aae
Michael Fatemi <myfatemi04@gmail.com>
2021-10-10 00:26:41 -0400

    add flex to segments

--- src/components/Segment.tsx
+++ src/components/Segment.tsx
@@ -3,7 +3,7 @@ import SegmentHeader from './SegmentHeader';
 
 export default function Segment({ title, content, imageUrl, infoUrl }) {
 	return (
-		<div style={{ textAlign: 'center', padding: '0px 17px' }}>
+		<div style={{ textAlign: 'center', padding: '0px 17px', flex: 1 }}>
 			<SegmentHeader>{title}</SegmentHeader>
 			<img src={imageUrl} alt={title} />
 			<p className='strong-paragraph'>{content}</p>
